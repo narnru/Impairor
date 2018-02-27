901df8324262afb0092e19916cc18c100abebb11
narnru <ivylanedenizen@gmail.com>
2018-02-27 20:45:24 +0300

    to make it work not only here

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+    QStringList paths = QCoreApplication::libraryPaths();
+    paths.append("platforms");
+    QCoreApplication::setLibraryPaths(paths);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
